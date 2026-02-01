baa69d69d2c4ff90e6ec2bf7beb3c997ad649d6d
Guilhem Vellut <guilhem.vellut@gmail.com>
2026-02-01 23:06:25 +0100

    Remove unnecessary exports

--- src/piqopiqo/fullscreen/__init__.py
+++ src/piqopiqo/fullscreen/__init__.py
@@ -1,32 +1,5 @@
-"""Fullscreen overlay module for displaying images at full resolution."""
-
-from .info_panel import (
-    ZoomDisplayState,
-    ZoomOverlayController,
-    should_show_zoom_overlay,
-)
 from .overlay import FullscreenOverlay
-from .pan import (
-    SideSpaces,
-    calculate_allowed_extra_from_current,
-    calculate_current_space,
-    calculate_effective_space_per_side,
-    is_image_visible,
-    update_allowed_extra_after_pan,
-)
-from .zoom import ZoomDirection, ZoomState
 
 __all__ = [
     "FullscreenOverlay",
-    "SideSpaces",
-    "ZoomDirection",
-    "ZoomDisplayState",
-    "ZoomOverlayController",
-    "ZoomState",
-    "calculate_allowed_extra_from_current",
-    "calculate_current_space",
-    "calculate_effective_space_per_side",
-    "is_image_visible",
-    "should_show_zoom_overlay",
-    "update_allowed_extra_after_pan",
 ]
--- src/piqopiqo/fullscreen_overlay.py
+++
@@ -1,6 +0,0 @@
-"""Backward compatibility: re-export from fullscreen/."""
-
-from .fullscreen.overlay import FullscreenOverlay
-from .fullscreen.zoom import ZoomDirection, ZoomState
-
-__all__ = ["FullscreenOverlay", "ZoomDirection", "ZoomState"]
