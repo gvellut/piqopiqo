1d3d4516633c90c4d3377f3b8d5023ccb16bd8f8
Guilhem Vellut <guilhem.vellut@gmail.com>
2026-02-14 18:04:03 +0100

    Fixed colored logs init

--- src/piqopiqo/utils.py
+++ src/piqopiqo/utils.py
@@ -7,11 +7,12 @@ import coloredlogs
 
 
 def setup_logging(logger=None):
-    fmt = "%(asctime)s %(levelname)-8s %(message)s"
+    fmt = "%(asctime)s %(name)s %(levelname)-8s %(message)s"
     datefmt = "%Y-%m-%d %H:%M:%S"
 
     # Always ensure the root logger is set to INFO
     root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
     coloredlogs.install(
         level=logging.INFO,
         logger=root_logger,
@@ -23,14 +24,7 @@ def setup_logging(logger=None):
 
     level_for_passed = logging.DEBUG if os.getenv("DEBUG") == "1" else logging.INFO
     if logger and logger is not root_logger:
-        coloredlogs.install(
-            level=level_for_passed,
-            logger=logger,
-            isatty=True,
-            fmt=fmt,
-            stream=sys.stdout,
-            datefmt=datefmt,
-        )
+        logger.setLevel(level_for_passed)
 
 
 class UpperStrEnum(StrEnum):
